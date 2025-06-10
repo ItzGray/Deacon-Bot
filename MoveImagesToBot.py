db71f86182db7c947add06bea437c7816883a22e
ItzGray <masonmustonen@gmail.com>
2025-06-10 16:28:58 -0500

    Actually made the changes work

--- MoveImagesToBot.py
+++ MoveImagesToBot.py
@@ -43,35 +43,35 @@ def move_images_to_bot():
         with open(output_file_path, "wb") as output_file:
             output_file.write(data)
     for file in root.iter_glob("GUI/Powers/*.dds"):
-        data = player_worlddata[file]
+        data = root[file]
         filename = file.split("/")[-1]
         output_file_path = output_path / filename
         print(f"Extracting {filename} from Root.wad")
         with open(output_file_path, "wb") as output_file:
             output_file.write(data)
     for file in root.iter_glob("GUI/Talents/*.dds"):
-        data = player_worlddata[file]
+        data = root[file]
         filename = file.split("/")[-1]
         output_file_path = output_path / filename
         print(f"Extracting {filename} from Root.wad")
         with open(output_file_path, "wb") as output_file:
             output_file.write(data)
     for file in shared_worlddata.iter_glob("GUI/Powers/*.dds"):
-        data = player_worlddata[file]
+        data = shared_worlddata[file]
         filename = file.split("/")[-1]
         output_file_path = output_path / filename
         print(f"Extracting {filename} from _Shared-WorldData.wad")
         with open(output_file_path, "wb") as output_file:
             output_file.write(data)
     for file in shared_worlddata.iter_glob("GUI/Powers/*.jpf"):
-        data = player_worlddata[file]
+        data = shared_worlddata[file]
         filename = file.split("/")[-1]
         output_file_path = output_path / filename
         print(f"Extracting {filename} from _Shared-WorldData.wad")
         with open(output_file_path, "wb") as output_file:
             output_file.write(data)
     for file in shared_worlddata.iter_glob("GUI/Doubloons/*.dds"):
-        data = player_worlddata[file]
+        data = shared_worlddata[file]
         filename = file.split("/")[-1]
         output_file_path = output_path / filename
         print(f"Extracting {filename} from _Shared-WorldData.wad")
